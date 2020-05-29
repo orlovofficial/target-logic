b7c93a98c361d6561a3fb2692047d94035f1eaa8
Иван <ivan@MacBook-Air-Ivan.local>
2020-05-30 02:09:01 +0700

    Добавнен счетчик новых постов

--- src/app/shared/components/widget/widget-user/widget-user.component.ts
+++ src/app/shared/components/widget/widget-user/widget-user.component.ts
@@ -28,13 +28,18 @@ export class WidgetUserComponent implements OnInit {
 
 
       this.analytics = data.edge_owner_to_timeline_media.edges.reduce((result, post) => {
+        const datePost = new Date(+post.taken_at_timestamp * 1000);
+        const dateNow = Date.now();
+        const day = (dateNow - datePost.getTime()) / (1000 * 3600 * 24);
         return {
           ...result,
           posts: result.posts + 1,
           allLikes: result.allLikes + post.edge_media_preview_like.count,
           allComments: result.allComments + post.edge_media_to_comment.count,
           allViews: post.video_view_count ? result.allViews + post.video_view_count : result.allViews,
-          video: post.video_view_count ? result.video + 1 : result.video
+          video: post.video_view_count ? result.video + 1 : result.video,
+          postsWeek: day < 7 ? result.postsWeek + 1 : result.postsWeek,
+          postsMonth: day < 30 ? result.postsMonth + 1 : result.postsMonth
         };
       }, newanalytics);
     });
